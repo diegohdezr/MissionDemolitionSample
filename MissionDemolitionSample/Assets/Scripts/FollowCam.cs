3a2257480d921d017f10494440c1230dd34ba1e4
diegohdezr <diego.hdez@hotmail.com>
2021-06-06 17:56:45 -0500

    basic follow camera functionallity
    still some details need to be pollished
    
    A. The camera sits at an initial position and doesn't move during Slingshot's aimingMode.
    B. After the projectile is launched, the camera follows it (with a little easing to make it feel smoother).
    C. As the camera moves up into the air, increase the Camera.orthographicSize to keep the ground in view.
    D. When the projectile comes to rest, the camera stops following it and returns to the initial position.

---
+++ MissionDemolitionSample/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject    POI; //the static point of interest
+    /*POI is the point of interest that the camera should follow (e.g., a Projectile). 
+     * As a static public field, the same value for POI is shared by all instances of the FollowCam class, 
+     * and POI can be accessed anywhere in code as FollowCam.POI. This makes it easy 
+     * for the Slingshot code to tell _MainCamera which Projectile to follow.
+
+     */
+
+    [Header("setDynamically")]
+    public float                camZ; //the desired Z pos of the camera
+
+    private void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        if (POI == null) return;//return if there is no POI
+
+        //get the position of the POI
+        Vector3 destination = POI.transform.position;
+        //force destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        //set the camera to destination
+        transform.position = destination;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
